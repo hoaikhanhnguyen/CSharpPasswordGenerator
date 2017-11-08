96cd591410770fb0020f0cdfc19ac92c0190bf61
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-11-07 20:39:00 -0800

    completed

--- CSharpPasswordGenerator/Program.cs
+++ CSharpPasswordGenerator/Program.cs
@@ -10,6 +10,23 @@ namespace CSharpPasswordGenerator
     {
         static void Main(string[] args)
         {
+            //This is a small program to randomly create a password that is all lower case and 10 characters long
+
+            //Using random Class...
+            //We use the ASCII code for the letters
+            //Since strings are immutable, we place them into an array then create the string
+            // because we cannot change the string once we create it
+            var random = new Random();
+
+            const int passwordLength = 10;
+            //declare const to avoid magic number
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++ )
+                buffer[i] = (char)('a' + random.Next(0,26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
         }
     }
 }
